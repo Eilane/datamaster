bc6807ce9a1bc23abe4f3d7eb264978104e84786
Eilane Santos <eilanedossantoscampos@gmail.com>
2025-10-05 02:26:12 -0300

    atualização no terraform

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -74,7 +74,10 @@ resource "azurerm_resource_group_template_deployment" "http_link" {
         "type": "Binary",
         "typeProperties": {
             "location": {
-                "type": "HttpServerLocation"
+                "type": "HttpServerLocation",
+            "compression": {
+                "type": "ZipDeflate"
+            }
             }
         }
     }
